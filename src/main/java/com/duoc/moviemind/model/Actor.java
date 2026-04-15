6a1a89da038217ee50bf23080420f0338e320a16
Nicolás Catalán <ncatalan111@gmail.com>
2026-04-15 13:36:53 -0400

    Creación de modelos pelicula, actor, usuario

---
+++ src/main/java/com/duoc/moviemind/model/Actor.java
@@ -0,0 +1,36 @@
+package com.duoc.moviemind.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name= "actores")
+
+public class Actor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idActor;
+
+    @NotBlank
+    private int nombre;
+
+    @NotNull
+    private int edad;
+
+    @NotBlank
+    private String nacionalidad;
+    
+}
---
+++ src/main/java/com/duoc/moviemind/model/Usuario.java
@@ -0,0 +1,31 @@
+package com.duoc.moviemind.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name= "usuarios")
+public class Usuario {
+    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idUsuario;
+
+    @NotBlank
+    private String NombreUsuario;
+
+    @NotBlank
+    private String correo;
+
+}
