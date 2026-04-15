0e7727dcce456cef1e547061ea68ca129b573cc9
Nicolás Catalán <ncatalan111@gmail.com>
2026-04-15 12:07:24 -0400

    Commit inicial: dependencias SpringBoot

---
+++ src/main/java/com/duoc/moviemind/MoviemindApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.moviemind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviemindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviemindApplication.class, args);
+	}
+
+}
