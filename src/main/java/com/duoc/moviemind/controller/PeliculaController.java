2cb104ca4a0bcdb0467d79eedd005c6fd0f4c7a7
Nicolás Catalán <ncatalan111@gmail.com>
2026-04-29 13:19:26 -0400

    Avance PeliculaController

--- src/main/java/com/duoc/moviemind/controller/PeliculaController.java
+++ src/main/java/com/duoc/moviemind/controller/PeliculaController.java
@@ -1,8 +1,70 @@
 package com.duoc.moviemind.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.duoc.moviemind.model.Pelicula;
+import com.duoc.moviemind.service.PeliculaService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/peliculas")
 public class PeliculaController {
+
+    @Autowired
+    PeliculaService peliculaService;
+
+    @GetMapping
+    public ResponseEntity<List<Pelicula>> listarPeliculas(){
+        return ResponseEntity.ok(peliculaService.getPeliculas());
+    }
+
+    @PostMapping
+    public ResponseEntity<Pelicula> agregarPelicula(@Valid @RequestBody Pelicula pelicula){
+        return ResponseEntity.status(HttpStatus.CREATED).body(peliculaService.savePelicula(pelicula));
+    }
+
+    @GetMapping
+    public ResponseEntity<Pelicula> buscarPelicula(@PathVariable int id){
+        Pelicula pelicula = peliculaService.getPeliculaId(id);
+        if(pelicula == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(pelicula);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Pelicula> actualizarPelicula(@PathVariable int id, @Valid @RequestBody Pelicula pelicula){
+        pelicula.setIdPelicula(id);
+        Pelicula actualizado = peliculaService.updatePelicula(pelicula);
+        if(actualizado == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(actualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarPelicula(@PathVariable int id){
+        peliculaService.detelePelicula(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    
+
+
+    
 
 
     
