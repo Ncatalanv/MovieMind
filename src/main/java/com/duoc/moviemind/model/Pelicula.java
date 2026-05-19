09e65ce2275b7003d9e5ffe01d6f927f8f354991
Nicolás Catalán <ncatalan111@gmail.com>
2026-05-19 13:09:10 -0400

    Se agregan constructores vacíos manualmente en Pelicula y Usuario

--- src/main/java/com/duoc/moviemind/model/Pelicula.java
+++ src/main/java/com/duoc/moviemind/model/Pelicula.java
@@ -18,14 +18,16 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name= "peliculas")
 public class Pelicula {
+
+    //Borré el NoArgsConstructor por un error
+    public Pelicula() {
+    }
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/duoc/moviemind/model/Usuario.java
+++ src/main/java/com/duoc/moviemind/model/Usuario.java
@@ -14,15 +14,15 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name= "usuarios")
 public class Usuario {
     
+    public Usuario() {
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
